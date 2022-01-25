06c5c7bb6d234b961ac50807460f39c2eb7999d4
aidilfahmiey <aidil.fahmiey@live.iium.edu.my>
2022-01-25 09:10:09 +0800

    latest update

--- App.js
+++ App.js
@@ -1,5 +1,4 @@
 //Author: Muhammad Aidil Fahmiey Bin Osman (1819369), Nur Sabrina Fasha binti Zaidi Sham (1813526)
-
 import * as React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
@@ -7,7 +6,6 @@ import { createStackNavigator } from "@react-navigation/stack";
 import Login from "./components/login";
 import Signup from "./components/signup";
 import AppNavigator from "./app/navigation/AppNavigator";
-import { CartProvider } from "./app/components/CartContext";
 
 const Stack = createStackNavigator();
 
@@ -35,45 +33,8 @@ function MyStack() {
 
 export default function App() {
   return (
-    <CartProvider>
-      <NavigationContainer theme={navigationTheme}>
-        {<AppNavigator />}
-      </NavigationContainer>
-    </CartProvider>
+    <NavigationContainer>
+      <MyStack />
+    </NavigationContainer>
   );
 }
-
-//-----------------------------
-// import React from "react";
-// import { NavigationContainer } from "@react-navigation/native";
-
-// import navigationTheme from "./app/navigation/navigationTheme";
-// import AppNavigator from "./app/navigation/AppNavigator";
-
-// export default function App() {
-//   return (
-//     <NavigationContainer theme={navigationTheme}>
-//       {<AppNavigator />}
-//     </NavigationContainer>
-//   );
-// }
-
-//------------------
-
-// import React from "react";
-// import MyOrderCard from "./app/components/MyOrderCard";
-// //import MyOrder from "./app/components/MyOrder";
-// import ColdBeverageMenu from "./app/screens/ColdBeverageMenu";
-// import Home from "./app/screens/Home";
-// import HotBeverageMenu from "./app/screens/HotBeverageMenu";
-// import IceBlendedMenu from "./app/screens/IceBlendedMenu";
-// import MenuCategory from "./app/screens/MenuCategory";
-// import MyAccount from "./app/screens/MyAccount";
-// //import Map from "./app/components/Map";
-// //import FetchListScreen from "./app/components/Profile";
-// import Map from "./app/components/Map";
-// //import MyOrder from "./app/screens/MyOrder";
-
-// export default function App() {
-//   return <Map />;
-// }
